861c4d4e2029cb1e8621447682668891c9f13b48
Eric <Eric9911Eric@gmail.com>
2023-09-26 08:15:56 +0800

    ex1.2

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,8 +1,8 @@
 def print_numbers():
     # Your task is to use a for loop to print numbers from 1 to 10.
     for i in range(1,11):
-    print(i)
-    i+1
+        print(i)
+        i+1
 pass
 
 def main():
